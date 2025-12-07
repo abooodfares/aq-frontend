ee66b370b1f430aaa51d4fcfce29604368071613
abooodfares <abooodfaresali@gmail.com>
2025-12-07 13:20:38 +0300

    refactor: remove index.ts barrel files from LeftSidebar, MapView, and RightSidebar component directories.

--- src/components/LeftSidebar/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as LeftSidebar } from './LeftSidebar';
-export { default as PropertyTypeToggle } from './PropertyTypeToggle';
-export { default as LocationSelectors } from './LocationSelectors';
-export { default as RangeFilter } from './RangeFilter';
-export { default as AdvancedFilters } from './AdvancedFilters';
